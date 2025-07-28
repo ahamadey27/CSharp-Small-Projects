5906071361e0fd064435a5dd11fce68e09b4e74a
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-28 18:59:06 -0400

    Improve console input handling and decision flow
    Updated input capture to use Console.ReadKey(true) for better user experience. Introduced a new variable `secondPath` to manage user choices more effectively in the narrative.

--- 01-Interactive-Story/01-Interactive-Story/Program.cs
+++ 01-Interactive-Story/01-Interactive-Story/Program.cs
@@ -1,23 +1,26 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Press any key to progress or 'a' or 'b' when prompted");
-Console.ReadKey();
+Console.ReadKey(true);
 
 Console.WriteLine("You are walking through a forest and you see a very strange dude");
-Console.ReadKey();
+Console.ReadKey(true);
 
 Console.WriteLine("He says, \"Bro, you gotta choose path A or Path B\"");
-var path = Console.ReadKey().Key;
+var path = Console.ReadKey(true).Key;
 
 if (path == ConsoleKey.A)
 {
     Console.WriteLine("You go down path A and there are a ton of rabbits just hanging out looking at you..you feel uneasy");
-    Console.ReadKey();
+    Console.ReadKey(true);
     Console.WriteLine("You see another guy who's chilling even harder");
     Console.WriteLine("He asks, \"You gotta choose path A or B again\"");
-    if (path == ConsoleKey.A)
+
+    var secondPath = Console.ReadKey(true).Key;
+
+    if (secondPath == ConsoleKey.A)
     {
         Console.WriteLine("You get to the end of the path and you grab a pint at the bar");
-        Console.ReadKey();
+        Console.ReadKey(true);
         return;
     }
     else

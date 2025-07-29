ab6107a85077bce66ed114b5f10de1f95cb68b13
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-29 11:47:55 -0400

    Add random number generation and display functionality
    This commit introduces functionality to display the initial value of `randomNumber`, initializes a `Random` object, and generates a random number between 0 and 99. The generated number is then printed to the console.

--- 02-RandomNumbers/02-RandomNumbers/Program.cs
+++ 02-RandomNumbers/02-RandomNumbers/Program.cs
@@ -2,6 +2,16 @@
 {
     static void Main(string[] args)
     {
-        int randomNumber = 0;
+        int randomDiceRoll = 0;
+        Console.WriteLine("Roll the dice!");
+        Console.WriteLine("\nPress any key to roll the dice");
+        Console.ReadKey(true); //Add true as argument to avoid character being added to next string
+
+        Random random = new Random(); //random initialization 
+        randomDiceRoll = random.Next(1, 7); //through numbers 11 and 6...7 will be excluded
+
+        Console.WriteLine($"Our random number is {randomDiceRoll}");
+
+
     }
 }

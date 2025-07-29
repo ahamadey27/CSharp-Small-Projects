352ff3d3dba16c5ef7721bbea6e454306aa5445f
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-29 12:00:56 -0400

    Enhance dice-rolling game logic and user prompts
    Updated the game to prompt the user to roll the dice repeatedly until their roll matches the computer's. Improved clarity of initial prompts and refined output messages for better user feedback.

--- 02-RandomNumbers/02-RandomNumbers/Program.cs
+++ 02-RandomNumbers/02-RandomNumbers/Program.cs
@@ -3,14 +3,28 @@
     static void Main(string[] args)
     {
         int randomDiceRoll = 0;
+
         Console.WriteLine("Roll the dice!");
-        Console.WriteLine("\nPress any key to roll the dice");
-        Console.ReadKey(true); //Add true as argument to avoid character being added to next string
+        Console.WriteLine("\nPress any key to roll the dice and keep going until you roll what the computer has generated");
 
-        Random random = new Random(); //random initialization 
-        randomDiceRoll = random.Next(1, 7); //through numbers 11 and 6...7 will be excluded
+        int diceRollComputer = 0;
+        Random randomComp = new Random();
+        diceRollComputer = randomComp.Next(1, 7); //Computers random number
 
-        Console.WriteLine($"Our random number is {randomDiceRoll}");
+        while (randomDiceRoll != diceRollComputer)
+        {
+            ConsoleKey keyPressed = Console.ReadKey(true).Key; //Add true as argument to avoid character being added to next string
+            Random random = new Random(); //random initialization 
+            randomDiceRoll = random.Next(1, 7); //through numbers 11 and 6...7 will be excluded
+
+            Console.WriteLine($"{randomDiceRoll} is not it!");
+
+            keyPressed = Console.ReadKey(true).Key;
+
+        }
+
+        Console.WriteLine($"You rolled {randomDiceRoll} which is matches the computer's roll of {diceRollComputer}");
+
 
 
     }

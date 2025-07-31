777c60f47e3aac0594b3c091e2a0966f6621ff52
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-31 13:35:06 -0400

    Add Rock-Paper-Scissors game to Program.cs
    Implement a simple Rock-Paper-Scissors game in the Main method. The game prompts the user for their move, generates a random move for the computer, and displays the corresponding string representations. Includes error handling for invalid player choices.

--- 03-RockPaperScissors/03-RockPaperScissors/Program.cs
+++ 03-RockPaperScissors/03-RockPaperScissors/Program.cs
@@ -2,6 +2,33 @@
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Rock paper scissors game");
+        Console.WriteLine("Choose 1 for rock - 2 for paper - 3 for scissors");
+        int playerMove = Convert.ToInt32(Console.ReadLine());
+
+        Random random = new Random();
+
+        int computerMove = random.Next(1, 4);
+
+        string playerMoveString = "";
+
+        switch (playerMove)
+        {
+            case 1: playerMoveString = "Rock"; break;
+            case 2: playerMoveString = "Paper"; break;
+            case 3: playerMoveString = "Scissors"; break;
+            default: Console.WriteLine("Wrong choice"); break;
+        }
+
+        string computerMoveString = "";
+
+        switch (computerMove)
+        {
+            case 1: computerMoveString = "Rock"; break;
+            case 2: computerMoveString = "Paper"; break;
+            case 3: computerMoveString = "Scissors"; break;
+            default: Console.WriteLine("Wrong choice"); break;
+        }
 
     }
 }

f5ac32ba717b3689b3ad318db72986075bdbad89
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-01 16:47:32 -0400

    Add initial project structure for Guess the Number game
    Created a new Visual Studio solution and project files.
    The project targets .NET 9.0 and includes a basic Program class
    that initializes a secret number randomly between 1 and 10.

---
+++ guess-the-number/guess-the-number/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+
+    //Initialize secret number to zero
+    int secretNumber = 0;
+
+    static void Main(string[] args)
+    {
+        Program program = new Program(); //Create reference instance for our Program class
+        Random random = new Random(); //initialize instance of a random class
+        program.secretNumber = random.Next(1, 10);
+
+
+    }
+}

fd138037ec0bd4f432bd278c9b2b59d605217b73
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-01 16:58:53 -0400

    Add number guessing feature to the program
    This commit introduces a new feature that prompts the user to guess a secret number between 1 and 10. A new method, `Guess()`, has been added to handle user input, convert it to an integer, and check the guess against the secret number. Feedback for the user's guess is set up but not yet fully implemented.

--- guess-the-number/guess-the-number/Program.cs
+++ guess-the-number/guess-the-number/Program.cs
@@ -10,6 +10,28 @@
         Random random = new Random(); //initialize instance of a random class
         program.secretNumber = random.Next(1, 10);
 
+        Console.WriteLine("Guess the number between 1 and 10");
+
+        void Guess()
+        {
+            int guess = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"You guessed {guess}");
+            if (guess < program.secretNumber)
+            {
+
+            }
+            else if (guess > program.secretNumber)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+        Guess();
+
+
 
     }
 }

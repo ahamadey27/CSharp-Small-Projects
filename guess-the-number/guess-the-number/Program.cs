3b2eeb0f2daaad4e72b6c9cff94b062048ad8720
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-01 17:08:07 -0400

    Add recursive guessing logic and win handling
    Introduced a new `Guess()` method for user input that provides feedback on guesses, calling itself recursively until the correct number is guessed. Added a `Win(int guess)` method to display a congratulatory message upon a correct guess. Removed the original `Guess()` method and updated logic to utilize the instance variable `secretNumber`.

--- guess-the-number/guess-the-number/Program.cs
+++ guess-the-number/guess-the-number/Program.cs
@@ -4,6 +4,33 @@
     //Initialize secret number to zero
     int secretNumber = 0;
 
+    void Guess()
+    {
+        int guess = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"You guessed {guess}");
+        if (guess < secretNumber)
+        {
+            Console.WriteLine($"No, the number is higher than {guess}. Guess again.");
+            Guess();
+        }
+        else if (guess > secretNumber)
+        {
+            Console.WriteLine($"No, the number is lower than {guess}. Guess again.");
+            Guess();
+
+        }
+        else
+        {
+            Win(guess);
+        }
+    }
+
+    void Win(int guess)
+    {
+
+        Console.WriteLine($"You guessed the correct number of {guess}");
+    }
+
     static void Main(string[] args)
     {
         Program program = new Program(); //Create reference instance for our Program class
@@ -11,28 +38,10 @@
         program.secretNumber = random.Next(1, 10);
 
         Console.WriteLine("Guess the number between 1 and 10");
+        program.Guess();
 
-        void Guess()
-        {
-            int guess = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"You guessed {guess}");
-            if (guess < program.secretNumber)
-            {
-                Console.WriteLine($"No, the number is higher than {guess}. Guess again.");
-                Guess();
-            }
-            else if (guess > program.secretNumber)
-            {
-                Console.WriteLine($"No, the number is lower than {guess}. Guess again.");
-                Guess();
 
-            }
-            else
-            {
 
-            }
-        }
-        Guess();
 
 
 

313d36c2dbfa5077040c1aa007849a7ed8840669
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-04 21:57:16 -0400

    Refactor game board display and add player handling
    - Introduced `playerCharacter` instance variable to track player symbols.
    - Simplified `DrawBoard` method for clearer board representation.
    - Added `Play` method to assign 'X' and 'O' based on player input.

--- 05-TicTacToe/05-TicTacToe/Program.cs
+++ 05-TicTacToe/05-TicTacToe/Program.cs
@@ -1,28 +1,31 @@
 ﻿public class Program
 {
     public static char[] boardPositions = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    public char playerCharacter = ' ';
 
     public static void DrawBoard()
     {
+        Console.WriteLine("     |     |     ");
+        Console.WriteLine("  {0}  |  {1}  |  {2}  ", boardPositions[0], boardPositions[1], boardPositions[2]);
+        Console.WriteLine("_____|_____|_____");
+        Console.WriteLine("     |     |     ");
+        Console.WriteLine("  {0}  |  {1}  |  {2}  ", boardPositions[3], boardPositions[4], boardPositions[5]);
+        Console.WriteLine("_____|_____|_____");
+        Console.WriteLine("     |     |     ");
+        Console.WriteLine("  {0}  |  {1}  |  {2}  ", boardPositions[6], boardPositions[7], boardPositions[8]);
+        Console.WriteLine("     |     |     ");
+    }
 
-        //Row 1
-        Console.WriteLine("  -------------------");
-        Console.WriteLine("  |     |     |     |");
-        Console.WriteLine("  | {0} | {1} | {2} |", boardPositions[0], boardPositions[1], boardPositions[2]);
-        Console.WriteLine("  |     |     |     |");
-        Console.WriteLine("  -------------------");
-        //Row 2
-        Console.WriteLine("  -------------------");
-        Console.WriteLine("  |     |     |     |");
-        Console.WriteLine("  | {0} | {1} | {2} |", boardPositions[3], boardPositions[4], boardPositions[5]);
-        Console.WriteLine("  |     |     |     |");
-        Console.WriteLine("  -------------------");
-        //Row 3
-        Console.WriteLine("  -------------------");
-        Console.WriteLine("  |     |     |     |");
-        Console.WriteLine("  | {0} | {1} | {2} |", boardPositions[6], boardPositions[7], boardPositions[8]);
-        Console.WriteLine("  |     |     |     |");
-        Console.WriteLine("  -------------------");
+    public void Play(int player, int input)
+    {
+        if (player == 1)
+        {
+            playerCharacter = 'X';
+        }
+        else if (player == 2)
+        {
+            playerCharacter = 'O';
+        }
     }
     public static void Main(string[] args)
     {

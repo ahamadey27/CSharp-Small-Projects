e8b86a0e415900d4ca4f28b5afa84aad3c9fa7c9
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-04 21:52:26 -0400

    Refactor program structure and add board display
    Reorganized the code into a new `Program` class.
    Replaced the original `Main` method with a new implementation that calls a `DrawBoard` method. Introduced a `boardPositions` array to represent game board positions and implemented the `DrawBoard` method to format and display the board layout.

--- 05-TicTacToe/05-TicTacToe/Program.cs
+++ 05-TicTacToe/05-TicTacToe/Program.cs
@@ -1,4 +1,39 @@
-﻿static void Main(string[] args)
+﻿public class Program
 {
+    public static char[] boardPositions = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+    public static void DrawBoard()
+    {
+
+        //Row 1
+        Console.WriteLine("  -------------------");
+        Console.WriteLine("  |     |     |     |");
+        Console.WriteLine("  | {0} | {1} | {2} |", boardPositions[0], boardPositions[1], boardPositions[2]);
+        Console.WriteLine("  |     |     |     |");
+        Console.WriteLine("  -------------------");
+        //Row 2
+        Console.WriteLine("  -------------------");
+        Console.WriteLine("  |     |     |     |");
+        Console.WriteLine("  | {0} | {1} | {2} |", boardPositions[3], boardPositions[4], boardPositions[5]);
+        Console.WriteLine("  |     |     |     |");
+        Console.WriteLine("  -------------------");
+        //Row 3
+        Console.WriteLine("  -------------------");
+        Console.WriteLine("  |     |     |     |");
+        Console.WriteLine("  | {0} | {1} | {2} |", boardPositions[6], boardPositions[7], boardPositions[8]);
+        Console.WriteLine("  |     |     |     |");
+        Console.WriteLine("  -------------------");
+    }
+    public static void Main(string[] args)
+    {
+        DrawBoard();
+        Console.ReadLine();
+    }
 
 }
+
+
+
+
+
+

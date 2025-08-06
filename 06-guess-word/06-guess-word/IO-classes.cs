cf709ab45f3fb6a565e152c93d8eb2f82a2cddb4
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-06 14:57:38 -0400

    Add initial project structure and IO functionality
    Created a new Visual Studio solution and project targeting .NET 9.0.
    Introduced an `IO_classes` file with methods for file I/O operations.
    Updated `Program.cs` to include a basic console output.

---
+++ 06-guess-word/06-guess-word/IO-classes.cs
@@ -0,0 +1,15 @@
+﻿namespace _06_guess_word
+{
+    public class IO_classes
+    {
+        public static void WriteTextExample()
+        {
+            string writeText = "Hello Text";
+            File.WriteAllText("filename.txt", writeText);
+            //newly created text is read and stored in variable
+            string readText = File.ReadAllText("filename.txt");
+            //The read text is outputted
+            Console.WriteLine(readText);
+        }
+    }
+}
